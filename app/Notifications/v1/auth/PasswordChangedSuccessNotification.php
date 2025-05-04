9b3b18b92aba2e4146876748ae9107a629400778
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-04 23:58:07 +0100

    added password successful reset notification

---
+++ app/Notifications/v1/auth/PasswordChangedSuccessNotification.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Notifications\v1\auth;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordChangedSuccessNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Password Changed Successfully')
+            ->line("Your password has been successfully updated.")
+            ->line("If you did not do this, please contact support immediately.");
+    }
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
