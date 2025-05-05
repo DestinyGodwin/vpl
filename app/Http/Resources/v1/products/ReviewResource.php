999959a5d416036ada72b4b71494d974caa7e631
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-05 20:26:59 +0100

    added review resource

---
+++ app/Http/Resources/v1/products/ReviewResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\v1\products;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    
+     public function toArray(Request $request): array
+     {
+         return [
+             'id' => $this->id,
+             'rating' => $this->rating,
+             'comment' => $this->comment,
+             'user' => [
+                 'name' => $this->user->first_name,
+                 'email' => $this->user->email,
+             ],
+             'created_at' => $this->created_at->diffForHumans(),
+         ];
+     }
+}
