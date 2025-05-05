a55a3938b6e7072223df076bdcc86f3fd945debf
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-05 19:59:09 +0100

    updated review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 class Review extends Model
 {
     use  HasUuids;
+    protected $fillable = ['product_id', 'user_id', 'rating', 'comment'];
+
+
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function product()
+{
+    return $this->belongsTo(Product::class);
+}
+
 }
