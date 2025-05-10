a3825f99b892acb5e264fed164aa189b59836dd2
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-10 11:11:52 +0100

    ipdated product request migration

--- database/migrations/2025_05_10_100720_create_product_request_images_table.php
+++ database/migrations/2025_05_10_100720_create_product_request_images_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('product_request_images', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_request_id')->constrained()->onDelete('cascade');
+            $table->string('path');
             $table->timestamps();
         });
     }
