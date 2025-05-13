aa040370076212569853a78366556d8c4685c613
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-13 22:51:04 +0100

    added middleware alreadly logged in to prevent loginn users from hitting the route again

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use App\Http\Middleware\EmailIsVerified;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\AlreadyLoggedIn;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
         $middleware->alias([
             'verified.otp' => EmailIsVerified::class,
+                'logged.in' => AlreadyLoggedIn::class,
+
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
