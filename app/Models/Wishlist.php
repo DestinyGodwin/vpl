05a33159515840d648ebde7f28ed6fbaecc3975e
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-14 23:32:46 +0100

    added wishlist model and blank  migration

---
+++ app/Models/Wishlist.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Wishlist extends Model
+{
+    use HasUuids, HasFactory;
+
+        protected $fillable = ['user_id', 'product_id'];
+        
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+}
