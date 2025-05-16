fd5a16d1eca245f2e16d696b1c26ac68f001066d
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-16 21:33:03 +0100

    updated store controller to use the pagination request

--- app/Http/Controllers/v1/stores/StoreController.php
+++ app/Http/Controllers/v1/stores/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\v1\stores;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\v1\stores\StoreService;
+use App\Http\Requests\v1\PaginationRequest;
 use App\Http\Requests\v1\stores\StoreRequest;
 use App\Http\Resources\v1\stores\StoreResource;
 use App\Http\Requests\v1\stores\UpdateStoreRequest;
@@ -45,47 +46,7 @@ class StoreController extends Controller
         return response()->json(['message' => 'Store deleted successfully.']);
     }
 
-    public function index(Request $request)
-    {
-        $perPage = $request->query('per_page');
-        return StoreResource::collection($this->storeService->getAll($perPage));
-    }
 
-    public function byType(Request $request, string $type)
-    {
-        $perPage = $request->query('per_page');
-        $stores = $this->storeService->getByType($type, $perPage);
-
-        if ($stores->isEmpty()) {
-            return response()->json(['message' => 'No stores found for the given type.'], 404);
-        }
-
-        return StoreResource::collection($stores);
-    }
-
-    public function byUniversity(Request $request, string $universityId, string $type = null)
-    {
-        $perPage = $request->query('per_page');
-        $stores = $this->storeService->getByUniversity($universityId, $type, $perPage);
-
-        if ($stores->isEmpty()) {
-            return response()->json(['message' => 'No stores found for the given university or type.'], 404);
-        }
-
-        return StoreResource::collection($stores);
-    }
-
-    public function byCountry(Request $request, string $countryId, string $type = null)
-    {
-        $perPage = $request->query('per_page');
-        $stores = $this->storeService->getByCountry($countryId, $type, $perPage);
-
-        if ($stores->isEmpty()) {
-            return response()->json(['message' => 'No stores found for the given country or type.'], 404);
-        }
-
-        return StoreResource::collection($stores);
-    }
 
     public function show($id)
     {
@@ -96,4 +57,44 @@ class StoreController extends Controller
 
         return new StoreResource($store);
     }
+ 
+
+public function index(PaginationRequest $request)
+{
+    return StoreResource::collection($this->storeService->getAll($request->getPerPage()));
+}
+
+public function byType(PaginationRequest $request, string $type)
+{
+    $stores = $this->storeService->getByType($type, $request->getPerPage());
+
+    if ($stores->isEmpty()) {
+        return response()->json(['message' => 'No stores found for the given type.'], 404);
+    }
+
+    return StoreResource::collection($stores);
+}
+
+public function byUniversity(PaginationRequest $request, string $universityId, string $type = null)
+{
+    $stores = $this->storeService->getByUniversity($universityId, $type, $request->getPerPage());
+
+    if ($stores->isEmpty()) {
+        return response()->json(['message' => 'No stores found for the given university or type.'], 404);
+    }
+
+    return StoreResource::collection($stores);
+}
+
+public function byCountry(PaginationRequest $request, string $countryId, string $type = null)
+{
+    $stores = $this->storeService->getByCountry($countryId, $type, $request->getPerPage());
+
+    if ($stores->isEmpty()) {
+        return response()->json(['message' => 'No stores found for the given country or type.'], 404);
+    }
+
+    return StoreResource::collection($stores);
+}
+
 }
