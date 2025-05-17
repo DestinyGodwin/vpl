f43b21549c5cd80234d3f560dca216b1dfd1d9a0
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-17 15:51:10 +0100

    added middleware to the route

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,6 @@ Route::prefix('v1')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->middleware('logged.in');
     Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
     Route::post('/reset-password', [AuthController::class, 'resetPassword']);
-    Route::post('/change-password', [AuthController::class, 'changePassword']);
 
     
 
@@ -38,6 +37,8 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:sanctum', 'verified.otp')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('profile', [AuthController::class, 'getProfile']);
+            Route::post('/change-password', [AuthController::class, 'changePassword']);
+
         Route::post('profile/update', [AuthController::class, 'updateProfile']);
         Route::apiResource('universities', UniversityController::class)->only('store');
          Route::get('wishlist', [WishlistController::class, 'index']);
