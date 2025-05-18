6c6b3ceefbff6eac02d9391c3ee2ceb2ad0a1517
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-18 14:40:19 +0100

    updated product request notification to include db

--- app/Notifications/v1/products/ProductRequestedNotification.php
+++ app/Notifications/v1/products/ProductRequestedNotification.php
@@ -3,37 +3,25 @@
 namespace App\Notifications\v1\products;
 
 use Illuminate\Bus\Queueable;
-use App\Models\ProductRequest;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use App\Models\ProductRequest;
 
 class ProductRequestedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
     public function __construct(public ProductRequest $productRequest)
     {
-        //
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail($notifiable)
+    public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
             ->subject('New Product Request')
@@ -45,15 +33,12 @@ class ProductRequestedNotification extends Notification implements ShouldQueue
             ->line('Thanks for using our platform!');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'title' => 'New Product Request',
+            'message' => "A user has requested a product that matches your store type: {$this->productRequest->name}",
+            'product_request_id' => $this->productRequest->id,
         ];
     }
 }
