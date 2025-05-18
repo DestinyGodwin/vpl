d7d06ad6b69ff9a142751373086e618433bc272e
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-18 19:00:21 +0100

    added product notiication service class

---
+++ app/Services/v1/products/NotificationService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services\v1\products;
+
+use Illuminate\Support\Facades\Auth;
+
+class NotificationService
+{
+    public function getAll($perPage = 20)
+    {
+        return Auth::user()->notifications()->latest()->paginate($perPage);
+    }
+
+    public function getUnread($perPage = 20)
+    {
+        return Auth::user()->unreadNotifications()->latest()->paginate($perPage);
+    }
+
+    public function markAsRead(string $notificationId): bool
+    {
+        $notification = Auth::user()->notifications()->findOrFail($notificationId);
+        $notification->markAsRead();
+
+        return true;
+    }
+
+    public function markAllAsRead(): void
+    {
+        Auth::user()->unreadNotifications->markAsRead();
+    }
+
+    public function delete(string $notificationId): bool
+    {
+        $notification = Auth::user()->notifications()->findOrFail($notificationId);
+        $notification->delete();
+
+        return true;
+    }
+
+    public function deleteAll(): void
+    {
+        Auth::user()->notifications()->delete();
+    }
+}
