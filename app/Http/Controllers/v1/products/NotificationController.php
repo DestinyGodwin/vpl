b0bbad7dc0a79773a27e520b472f9cce2247bb0a
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-18 19:02:36 +0100

    added product notification controller

---
+++ app/Http/Controllers/v1/products/NotificationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\v1\products;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\v1\products\NotificationService;
+
+class NotificationController extends Controller
+{
+   
+    public function __construct(protected NotificationService $notificationService) {}
+
+    public function index(Request $request)
+    {
+        return response()->json([
+            'notifications' => $this->notificationService->getAll()
+        ]);
+    }
+
+    public function unread(Request $request)
+    {
+        return response()->json([
+            'notifications' => $this->notificationService->getUnread()
+        ]);
+    }
+
+    public function markAsRead(Request $request, string $id)
+    {
+        $this->notificationService->markAsRead($id);
+
+        return response()->json(['message' => 'Notification marked as read.']);
+    }
+
+    public function markAllAsRead()
+    {
+        $this->notificationService->markAllAsRead();
+
+        return response()->json(['message' => 'All notifications marked as read.']);
+    }
+
+    public function destroy(string $id)
+    {
+        $this->notificationService->delete($id);
+
+        return response()->json(['message' => 'Notification deleted.']);
+    }
+
+    public function destroyAll()
+    {
+        $this->notificationService->deleteAll();
+
+        return response()->json(['message' => 'All notifications deleted.']);
+    }
+}
