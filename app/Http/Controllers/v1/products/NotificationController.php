348f7aa661ccf2ee038cfd6121cbd6e6baf8f625
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-21 00:13:25 +0100

    updated notification controller

--- app/Http/Controllers/v1/products/NotificationController.php
+++ app/Http/Controllers/v1/products/NotificationController.php
@@ -4,32 +4,13 @@ namespace App\Http\Controllers\v1\products;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\NotificationResource;
 use App\Services\v1\products\NotificationService;
 
 class NotificationController extends Controller
 {
    
     public function __construct(protected NotificationService $notificationService) {}
-
-    public function index(Request $request)
-    {
-        return response()->json([
-            'notifications' => $this->notificationService->getAll()
-        ]);
-    }
-public function show(string $id)
-{
-    $notification = $this->notificationService->getOne($id);
-
-    return response()->json(['notification' => $notification]);
-}
-    public function unread(Request $request)
-    {
-        return response()->json([
-            'notifications' => $this->notificationService->getUnread()
-        ]);
-    }
-
     public function markAsRead(Request $request, string $id)
     {
         $this->notificationService->markAsRead($id);
@@ -57,4 +38,27 @@ public function show(string $id)
 
         return response()->json(['message' => 'All notifications deleted.']);
     }
+    public function index(Request $request)
+{
+    $notifications = $this->notificationService->getAll();
+    return response()->json([
+        'notifications' => NotificationResource::collection($notifications)
+    ]);
+}
+
+public function unread(Request $request)
+{
+    $notifications = $this->notificationService->getUnread();
+    return response()->json([
+        'notifications' => NotificationResource::collection($notifications)
+    ]);
+}
+
+public function show(string $id)
+{
+    $notification = $this->notificationService->getOne($id);
+    return response()->json([
+        'notification' => new NotificationResource($notification)
+    ]);
+}
 }
