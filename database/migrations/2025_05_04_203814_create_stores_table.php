d802a674c3d66fa550e8f97a2c62e713f33f6c6e
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-20 11:29:42 +0100

    added restrict to product and stores table

--- database/migrations/2025_05_04_203814_create_stores_table.php
+++ database/migrations/2025_05_04_203814_create_stores_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('stores', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
-            $table->foreignUuid('university_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('university_id')->constrained()->onDelete('restrict');
             $table->enum('type', ['regular', 'food']);
             $table->string('name');
             $table->string('image')->nullable();
--- database/migrations/2025_05_04_234151_create_products_table.php
+++ database/migrations/2025_05_04_234151_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('store_id')->constrained()->onDelete('cascade');
-            $table->foreignUuid('category_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('category_id')->constrained()->onDelete('restrict');
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
