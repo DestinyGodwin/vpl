5f91686be70aabfc3fcaf5fdb2f52593ede7faf3
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-21 08:47:32 +0100

    addeed admin service

---
+++ app/Services/v1/auth/AdminService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\v1\auth;
+
+use App\Models\User;
+
+class AdminService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function getAllUsers($perPage = 50)
+    {
+        return User::with('university')->paginate($perPage);
+    }
+
+    public function getUsersByUniversity($universityId, $perPage = 50)
+    {
+        return User::where('university_id', $universityId)
+                   ->with('university')
+                   ->paginate($perPage);
+    }
+
+    public function getUsersByState($state, $perPage = 50)
+    {
+        return User::whereHas('university', function ($q) use ($state) {
+            $q->whereRaw('LOWER(state) = ?', [strtolower($state)]);
+        })->with('university')->paginate($perPage);
+    }
+
+    public function getUsersByCountry($country, $perPage = 50)
+    {
+        return User::whereHas('university', function ($q) use ($country) {
+            $q->whereRaw('LOWER(country) = ?', [strtolower($country)]);
+        })->with('university')->paginate($perPage);
+    }
+
+    public function notifyUsers($users, $title, $message)
+    {
+        foreach ($users as $user) {
+            $user->notify(new GenericNotification($title, $message));
+        }
+    }
+}
