513cd76ada85e7b530201f2f5aacae939b50758b
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-05-22 20:40:19 +0100

    updated product request notification

--- app/Notifications/v1/products/ProductRequestedNotification.php
+++ app/Notifications/v1/products/ProductRequestedNotification.php
@@ -12,34 +12,48 @@ class ProductRequestedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    public function __construct(public ProductRequest $productRequest)
+    public ProductRequest $productRequest;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(ProductRequest $productRequest)
     {
+        $this->productRequest = $productRequest->fresh(); 
     }
 
+    /**
+     * Get the notification's delivery channels.
+     */
     public function via(object $notifiable): array
     {
         return ['mail', 'database'];
     }
 
+    /**
+     * Get the mail representation of the notification.
+     */
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
             ->subject('New Product Request')
             ->greeting("Hello {$notifiable->first_name},")
             ->line("A user has requested a product on Vplaza.")
-            ->line("Log in to your account and navigate to 'Alert' to view the notification")
+            ->line("Log in to your account and navigate to 'Alert' to view the notification.")
             ->line("Product: {$this->productRequest->name}")
             ->line("Description: {$this->productRequest->description}")
-         ->action('View Request', config('app.url') . '/product-requests/' . $this->productRequest->id)
+            ->action(                'View Request',url('/product-requests/' . $this->productRequest->getKey()))
             ->line('Thanks for using our platform!');
     }
 
+    /**
+     * Get the array representation of the notification for database storage.
+     */
     public function toArray(object $notifiable): array
     {
-  
         return [
             'title' => 'New Product Request',
-            'message' => "A user has requested a product that you may have, {$this->productRequest->name}",
+            'message' => "A user has requested a product that you may have: {$this->productRequest->name}",
             'product_request_id' => $this->productRequest->id,
         ];
     }
