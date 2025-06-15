46fc099f87213686dd8a7bf99107b99108563408
DestinyGodwin <destinyogodwin.dg@gmail.com>
2025-06-15 21:17:46 +0100

    added global 404 handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,29 +1,38 @@
 <?php
 
+use Illuminate\Http\Request;
 use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Application;
 use App\Http\Middleware\AlreadyLoggedIn;
 use App\Http\Middleware\EmailIsVerified;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->alias([
             'verified.otp' => EmailIsVerified::class,
-                'logged.in' => AlreadyLoggedIn::class,
-                    'admin' => IsAdmin::class,
+            'logged.in' => AlreadyLoggedIn::class,
+            'admin' => IsAdmin::class,
 
 
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Record not found.'
+                ], 404);
+            }
+        });
     })->create();
